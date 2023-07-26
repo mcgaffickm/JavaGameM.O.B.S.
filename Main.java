65d3d1474e4dc7f3d8406d69904f8fe4e0cf1be8
mcgaffickm <mjmcgaffick@yahoo.com>
2023-07-26 02:18:39 -0400

    Adds in the basic classes with standard constructors and variables

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main
 {
     public static void main(String args[])
     {
-        System.out.println("Hello World");
+        System.out.println("Welcome to Mechanized Opponent Battle Simulator (M.O.B.S.)");
     }
 }
