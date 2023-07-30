8a9c481468eff56fbd46013d32bddf0da737dd5f
mcgaffickm <mjmcgaffick@yahoo.com>
2023-07-30 01:32:43 -0400

    Organizes and sets up the outlines for a few more classes

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("Welcome to Mechanized Opponent Battle Simulator (M.O.B.S.)");
+        
+    }
+}
