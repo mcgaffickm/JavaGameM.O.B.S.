1efedb0f65fe397acc0d10850cacdd2e0b57d79c
mcgaffickm <mjmcgaffick@yahoo.com>
2023-07-30 01:49:33 -0400

    Adds structure to battle classes and basic outline/variables/methods

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main
     public static void main(String args[])
     {
         System.out.println("Welcome to Mechanized Opponent Battle Simulator (M.O.B.S.)");
-        
+        BattleManager battleManager = new BattleManager();
+            
     }
 }
