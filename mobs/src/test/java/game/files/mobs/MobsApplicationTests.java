4b1c977dbb38496f3f11503aa468ed7788a25de0
mcgaffickm <michael.mcgaffick@gmail.com>
2023-07-31 01:24:57 -0400

    Reorgnaziation

---
+++ mobs/src/test/java/game/files/mobs/MobsApplicationTests.java
@@ -0,0 +1,13 @@
+package game.files.mobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
